a7ea9cf75aab5f95a2d393891c84220a847e5861
legrand <tleguede@gmail.com>
2023-04-10 13:16:18 +0000

    fix

--- api/notify.ts
+++ api/notify.ts
@@ -8,19 +8,19 @@ export default async function handler(
 
     corsHandler(request, response, async () => {
 
-    const body = request.body;
+        const body = JSON.parse(request.body);
 
-      const snap = await  messaging.sendMulticast({
-            tokens:[...body?.tokens],
-            data:{
-                title:body?.title,
-                description:body?.description,
-                ...body?.data ,
+        const snap = await messaging.sendMulticast({
+            tokens: [...body?.tokens],
+            data: {
+                title: body?.title,
+                description: body?.description,
+                ...body?.data,
 
             },
-            notification:{ 
-                title:body?.title,
-                body:body?.description,
+            notification: {
+                title: body?.title,
+                body: body?.description,
             }
         });
 
@@ -29,7 +29,7 @@ export default async function handler(
             query: request.query,
             cookies: request.cookies,
             headers: request.headers,
-            result:snap
+            result: snap
         });
     })
 
