bf030a74fb5bf268e443846bd0fd488720fdacca
legrand <tleguede@gmail.com>
2023-04-10 13:58:52 +0000

    error

--- api/notify.ts
+++ api/notify.ts
@@ -39,6 +39,7 @@ export default async function handler(
                 query: request.query,
                 cookies: request.cookies,
                 headers: request.headers,
+                error
             });
         }
 
